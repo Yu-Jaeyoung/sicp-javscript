9099d4080e28f7a64becb5863aaf59a1cda651b2
Yu-Jaeyoung <jackyu0721@gmail.com>
2025-12-03 16:53:16 +0900

    refactor fermat-test example

--- chapter01/functions-and-the-processes-they-generate/testing-for-primality/fermat-test/example.js
+++ chapter01/functions-and-the-processes-they-generate/testing-for-primality/fermat-test/example.js
@@ -2,10 +2,9 @@ function expmod(base, exp, m) {
     return exp === 0
         ? 1
         : is_even(exp)
-            ? square(expmod(base, exp / 2)) % m
+            ? square(expmod(base, exp / 2, m)) % m
             : (base * expmod(base, exp - 1, m)) % m;
 }
-
 
 function fermat_test(n) {
     function try_it(a) {
